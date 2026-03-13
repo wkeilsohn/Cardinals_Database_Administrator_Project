ff84de2c7c32a8bf1b3f5f78b3723e4bcfe23545
wkeilsohn <kingwilliam42@gmail.com>
2026-03-13 21:16:22 +0900

    DB Conn

--- Set_Up_Code/data_loader.py
+++ Set_Up_Code/data_loader.py
@@ -5,6 +5,11 @@
 import numpy as np
 import pandas as pd
 import os
+from dotenv import load_dotenv
+import psycopg2 as psy
+
+# Load in Secrets
+load_dotenv()
 
 # Define Variables
 
@@ -24,11 +29,33 @@ dba_project_data_df = pd.read_excel(dba_project_data)
 # weather_data # TBD
 
 ## PostgreSQL Connection Related
-address = "127.0.0.1"
+address = "127.0.0.1" # Yes, in prod you should hide all of these, but this is a quick project and I'm running it on my local machine so it is what it is. 
 port = "5432"
+db_name = "postgres"
+user_name = "postgres"
+password = os.getenv("PASSWORD") # Too spicy not to hide. 
 
 # Declare Functions
+def create_post_conn():
+	global address
+	global port
+	global db_name
+	global user_name
+	global password
+	conn = psy.connect(host=address, database=db_name, user=user_name, password=password, port=int(port))
+	return conn
+
+def check_post_conn(conn):
+	if conn.closed == 0:
+		print("Connection is Open")
+		return conn
+	else:
+		return create_post_conn()
 
 # Run Application
 if __name__ == "__main__":
-	print(dba_project_data_df)
+	conn = create_post_conn()
+	conn = check_post_conn(conn)
+
+
+	conn.close() # Just good etiquite. 
